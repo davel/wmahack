016035a30a849131e32af2c848b5e614a2883f50
Dave Lambley <github@davel.me.uk>
2010-12-09 21:46:27 +0000

    Get a class object function.

--- wmahack.c
+++ wmahack.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 #include <windows.h>
 
+void bail(const char *err) {
+	fprintf(stderr, "%s", err);
+	exit(1);
+}
+
 int main(int argc, char *argv[]) {
 	HMODULE hDll;
-	void *func;
+	void *DllGetClassObject;
 
 	printf("Hello world\n");
 
 	hDll = LoadLibraryA("/usr/lib/win32/wma9dmod.dll");
 
-	func = GetProcAddress(hDll, "DllGetClassObject");
+	DllGetClassObject = GetProcAddress(hDll, "DllGetClassObject");
+	if (!DllGetClassObject) bail("Bad COM DLL");
+	
 
 	return 0;
 }
